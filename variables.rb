4b9146051a119d7324511e12c84a30db83ccf078
Katy Harrod <m.catherine.harrod@gmail.com>
2021-07-30 16:44:54 -0600

    Add drop command

---
+++ variables.rb
@@ -0,0 +1,17 @@
+animal = "leopard"
+is_cuddly = false
+variable = "seal"
+num_8 = 5
+num_3 = 10
+
+num_3 / num_8
+
+quotient = "#{num_3 / num_8}"
+
+puts quotient
+
+favorite_films = ["Legally Blonde", "Old Boy", "Shrek"]
+
+puts favorite_films.drop(1)
+
+puts animal.length
