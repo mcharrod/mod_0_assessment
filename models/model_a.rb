f36a9d5ac0cb77473651d3ecb9e189e81d1139d4
Katy Harrod <m.catherine.harrod@gmail.com>
2021-07-30 16:34:10 -0600

    Correct formatting

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,11 +1,11 @@
 LAPTOP = {
-  screens = 1
-  letters_on_keyboard = 26
-  has_keyboard = true
-  has_empty_trash = false
-  user = "Katy"
-  laptop_brand = "Mac"
-  apps_on_laptop = ["Password Manager", "ad blocker", "maps"]
-  sites_visited = ["google", "mayo clinic", "wikipedia"]
+  screens => 1,
+  letters_on_keyboard => 26,
+  has_keyboard => true,
+  has_empty_trash => false,
+  user => "Katy",
+  laptop_brand => "Mac",
+  apps_on_laptop => ["Password Manager", "ad blocker", "maps"],
+  sites_visited => ["google", "mayo clinic", "wikipedia"]
 
 }
---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+HOUSE = {
+  street_name => "Star Creek",
+  closest_store => "Walmart",
+  number_of_tenants => 4,
+  number_of_windows => 10,
+  is_haunted => false,
+  has_lawn => true,
+  rooms => ["kitchen", "bathroom", "bedroom", "office"],
+  names_of_neighbors => ["Mrs. Moon", "Mr. Hendrick", "Dracula"]
+}
